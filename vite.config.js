49707d826bec23bedc3c3932f01c04340d900933
unknown <75127793+propiesek123@users.noreply.github.com>
2023-01-05 07:14:26 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+  ],
 };
 
 export default config;
